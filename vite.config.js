0d6316035cf2763d06c168434c9de19e1b1c3955
Louis de Tilly <louisdtb@gmail.com>
2023-11-05 00:16:33 -0400

    added gh-pages base to vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://louisdtb.github.io/wikipedia-country-map/",
   plugins: [react(), svgrPlugin()],
   css: { postcss: { plugins: [] } },
 });
